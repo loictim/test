df18809e0882da906cb2e164fd79d740cb6af94a
loictim <loictim@users.noreply.github.com>
2021-02-03 10:47:48 +0100

    test file

---
+++ functions/printhello/lambda_function.py
@@ -0,0 +1,12 @@
+import json
+import logging
+import traceback
+import psycopg2
+
+version='1.0.6'
+
+def lambda_handler(event, context):
+  return {
+            'statusCode': 200,
+            'body' : 'Hello'
+        }
