07edd4ee523806a7fd51a7a59f5ea45e38ee505d
loictim <loictim@users.noreply.github.com>
2021-02-03 14:00:38 +0100

    Update lambda_functions.py

--- functions/printhello2/lambda_functions.py
+++ functions/printhello2/lambda_functions.py
@@ -1,1 +1,12 @@
-csxy
+import json
+import logging
+import traceback
+import psycopg2
+
+version='1.0.6'
+
+def lambda_handler(event, context):
+  return {
+            'statusCode': 200,
+            'body' : 'Hello2'
+        }
